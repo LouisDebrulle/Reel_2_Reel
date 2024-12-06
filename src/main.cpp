890108e2ffa0c8a77c34ea4082bb52323d7048c9
Louis Debrulle <p121525@ge.polymtl.ca>
2024-12-06 15:02:57 -0500

    change serial output

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "board.h"
 #include "pin.h"
 #include "mv_average_filter.h"
+#include "PID.h"
 
 
 unsigned long lDt = 100*1e3;
@@ -13,11 +14,11 @@ const int filter_size = 10;
 
 Board _board;
 Motor _motor1(_board);
-
+PID _pid(100,0.01,0,0, 10000);
 state_machine kernel(_board);
 
-double times[] = {0, 100*1e3}; 
-double speeds[] = {0, 2};                                                     
+double times[] = {0, 10*1e3};//{0, 10*1e3, 10*1e3, 20*1e3, 20*1e3, 30*1e3, 30*1e3, 40*1e3}; 
+double speeds[] = {1,1}; //{0.3, 0.3, 0.6, 0.6, 0.8, 0.8, 1, 1};                                                     
 lin_speed_curve _speed_curve(times, speeds, sizeof(times)/sizeof(times[0])); 
 
 mv_average_filter<double,filter_size> speed_filter;
@@ -57,18 +58,24 @@ void check_status(){
     double time = kernel.get_time();
     double speed =_board._encoder.get_speed();
     speed_filter.push(speed);
+    double speed_smooth = speed_filter.get_average();
     
     double speed_des = _speed_curve.getSpeedAtTime(time);
+    //float dc = _pid.output(speed_des, speed_smooth);
     int dc = _motor1.get_dc(speed_des);
-    _motor1.set_speed(dc, speed_des);
+    _motor1.set_speed(int(dc), 0);
 
-    // double pos = _board._pos_sensor.get_pos();
+    double pos = _board._pos_sensor.get_pos();
 
     Serial.print(time/1000);
     Serial.print(" ,");
     Serial.print(speed_des);
     Serial.print(" ,");
-    Serial.println(speed_filter.get_average());
+    Serial.print(speed_smooth);
+    Serial.print(" ,");
+    Serial.print(double(dc)/ICR4 *100); // 
+    Serial.print(" ,");
+    Serial.println(pos);
     
    
     break;
